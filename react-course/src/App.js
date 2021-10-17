198e15bcbd71657a035a5fa79ba66f3110557369
Matthew Vandenberg <mv5903@gmail.com>
2021-10-16 19:46:00 -0700

    Creating Components and Props

--- react-course/src/App.js
+++ react-course/src/App.js
@@ -1,7 +1,14 @@
+import Todo from './components/Todo.js';
+
 function App() {
-  return (
-    <div>Hello!</div>
-  );
+    return (
+        <div>
+            <h1>My Todos</h1>
+            <Todo text="Learn React"/>
+            <Todo text="Master React" />
+            <Todo text="Explore Full React Course" />
+        </div>
+    );
 }
 
 export default App;
--- react-course/src/index.js
+++ react-course/src/index.js
@@ -1,11 +1,13 @@
+//The starting point of the React Project
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './App'; //js file extension can be omitted in react
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+    <App />,
+    document.getElementById('root')
 );
 
 
